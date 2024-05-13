082c7b41dc00336c0e07842b0f8c654b67f42567
John Kennith Mirano <john.mirano@ibm.com>
2024-05-13 21:02:41 +0800

    update: created eval for expense line graph data

--- src/app/pages/expenses/expenses.page.ts
+++ src/app/pages/expenses/expenses.page.ts
@@ -72,7 +72,7 @@ export class ExpensesPage implements OnInit, AfterContentInit {
   // Pie
   pieData: any[] = [];
   pieOptions: any = {
-    title: 'Expenses',
+    title: 'Expenses - Categories',
     resizable: true,
     height: '400px',
     pie: {
@@ -84,121 +84,18 @@ export class ExpensesPage implements OnInit, AfterContentInit {
     theme: 'g90',
   };
   // -- Line
-  lineData: any = [
-    {
-      group: 'Dataset 1',
-      key: 'Qty',
-      value: 34200,
-    },
-    {
-      group: 'Dataset 1',
-      key: 'More',
-      value: 23500,
-    },
-    {
-      group: 'Dataset 1',
-      key: 'Sold',
-      value: 53100,
-    },
-    {
-      group: 'Dataset 1',
-      key: 'Restocking',
-      value: 42300,
-    },
-    {
-      group: 'Dataset 1',
-      key: 'Misc',
-      value: 12300,
-    },
-    {
-      group: 'Dataset 2',
-      key: 'Qty',
-      value: 34200,
-    },
-    {
-      group: 'Dataset 2',
-      key: 'More',
-      value: 53200,
-    },
-    {
-      group: 'Dataset 2',
-      key: 'Sold',
-      value: 42300,
-    },
-    {
-      group: 'Dataset 2',
-      key: 'Restocking',
-      value: 21400,
-    },
-    {
-      group: 'Dataset 2',
-      key: 'Misc',
-      value: 0,
-    },
-    {
-      group: 'Dataset 3',
-      key: 'Qty',
-      value: 41200,
-    },
-    {
-      group: 'Dataset 3',
-      key: 'More',
-      value: 18400,
-    },
-    {
-      group: 'Dataset 3',
-      key: 'Sold',
-      value: 34210,
-    },
-    {
-      group: 'Dataset 3',
-      key: 'Restocking',
-      value: 1400,
-    },
-    {
-      group: 'Dataset 3',
-      key: 'Misc',
-      value: 42100,
-    },
-    {
-      group: 'Dataset 4',
-      key: 'Qty',
-      value: 22000,
-    },
-    {
-      group: 'Dataset 4',
-      key: 'More',
-      value: 1200,
-    },
-    {
-      group: 'Dataset 4',
-      key: 'Sold',
-      value: 9000,
-    },
-    {
-      group: 'Dataset 4',
-      key: 'Restocking',
-      value: 24000,
-      audienceSize: 10,
-    },
-    {
-      group: 'Dataset 4',
-      key: 'Misc',
-      value: 3000,
-      audienceSize: 10,
-    },
-  ];
+  lineData: any[] = [];
   lineOptions: any = {
-    title: 'Income/Savings',
+    title: 'Expenses - Expected/Actual',
     axes: {
       bottom: {
-        title: '2019 Annual Sales Figures',
+        title: '2024 Annual Expenses',
         mapsTo: 'key',
         scaleType: 'labels',
       },
       left: {
         mapsTo: 'value',
-        title: 'Conversion rate',
+        title: 'Cost (₱)',
         scaleType: 'linear',
       },
     },
@@ -395,6 +292,7 @@ export class ExpensesPage implements OnInit, AfterContentInit {
             this.tableModel.pageLength = response?.meta?.displayItem || 10;
             this.tableModel.totalDataLength = response?.meta?.total || 0;
             this.evalExpensePieData();
+            this.evalExpenseLineData(data);
           } else {
             this.tableModel.data = [];
             this.tableModel.currentPage = response?.meta?.page || 1;
@@ -452,6 +350,24 @@ export class ExpensesPage implements OnInit, AfterContentInit {
       return newRow;
     });
     return tableData;
+  }
+
+  evalExpenseLineData(data: any) {
+    const expectedData = data.map((d: any) => {
+      return {
+        group: 'Expected',
+        key: formatDate(new Date(d.due), 'MMM-d', 'en-us'),
+        value: d.expected,
+      };
+    });
+    const actualData = data.map((d: any) => {
+      return {
+        group: 'Actual',
+        key: formatDate(new Date(d.due), 'MMM-d', 'en-us'),
+        value: d.actual,
+      };
+    });
+    this.lineData = [...expectedData, ...actualData];
   }
 
   overflowOnClick = (event: any) => {
